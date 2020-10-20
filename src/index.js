4da7c363fa908a7b84c6f8ebcaf8d783b6d620ef
Alex Avtushko <pseledkin@yandex.by>
2020-10-20 19:04:08 +0300

    feat: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let splittedStr = str.split('');
+    for (let i = 0; i < splittedStr.length ; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if ((splittedStr[i] === bracketsConfig[j][0]) && (splittedStr[i+1]=== bracketsConfig[j][1])) {
+          splittedStr.splice(i, 2);
+          i=-1;
+        }
+      }
+    }
+    return !splittedStr.length;
 }
